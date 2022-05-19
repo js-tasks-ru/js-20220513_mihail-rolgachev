cc803f36612b0787f4499e3d1475fd91cfa25fa9
misha <misha_r82@mail.ru>
2022-05-19 09:09:53 +0300

    1-я итерация

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,6 +4,20 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
+function compareDesc(fist, second)
+{
+    if (fist > second) return 1;
+    if (fist === second) return 0;
+    return -1;
+}
 export function sortStrings(arr, param = 'asc') {
 
+switch (param)
+{
+    case 'asc': return arr.sort();
+    case 'desc': return arr.sort(compareDesc);
+    default: throw new Error("Неверное значение параметра сортировки " + param);
 }
+    
+}
+Console.Log(sortStrings(['b', 'a', 'c'], 'asc'));
